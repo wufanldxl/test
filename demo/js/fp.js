d6950a834fad1cdf2c5d6954c1ce0567d81c3356
wufan <fanwu@caixin.com>
2019-04-09 18:07:26 +0800

    测试提交

--- demo/js/fp.js
+++ demo/js/fp.js
@@ -1,4 +1,4 @@
 app.formdata.title='财新传媒有限公司';
 app.formdata.identification='911100006976705645';
-app.formdata.email=myEmail;
-app.formdata.phone=myTel
+app.formdata.email=document.myEmail;
+app.formdata.phone=document.myTel;
