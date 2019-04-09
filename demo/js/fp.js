b00a044e414504eba4fd15372c6fd45e56260f37
wufan <fanwu@caixin.com>
2019-04-09 18:17:37 +0800

    测试提交

--- demo/js/fp.js
+++ demo/js/fp.js
@@ -1,5 +1,5 @@
-console.log(document.myEmail,document.myTel)
+console.log(myEmail,myTel)
 app.formdata.title='财新传媒有限公司';
 app.formdata.identification='911100006976705645';
-app.formdata.email=document.myEmail;
-app.formdata.phone=document.myTel;
+app.formdata.email=myEmail;
+app.formdata.phone=myTel;
